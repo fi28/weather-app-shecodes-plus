80321cf66ce5c5deb81356e3686dc4f888b69da5
fi28 <110752627+fi28@users.noreply.github.com>
2022-10-30 19:11:33 +1000

    added unit conversion. started adding forecast API. edited some CSS and the time functions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 //we want day and and time
+function farenhiteConversion(event) {
+  event.preventDefault;
 
-function currentTime(now) {
+  let currentTemp = document.querySelector("#current-temperature-value");
+  currentFarenhiteTemp = Math.round((celsiusTemp * 9) / 5 + 32);
+  currentTemp.innerHTML = `${currentFarenhiteTemp}`;
+  celsiusLink.classList.remove("active");
+  farenhiteLink.classList.add("active");
+}
+function celsiusConversion(event) {
+  event.preventDefault;
+  let currentTemp = document.querySelector("#current-temperature-value");
+  currentTemp.innerHTML = `${celsiusTemp}`;
+  celsiusLink.classList.add("active");
+  farenhiteLink.classList.remove("active");
+}
+
+function currentTime(time) {
+  let now = new Date(time);
+  // console.log(now);
   let days = [
     "Sunday",
     "Monday",
@@ -22,16 +40,33 @@ function currentTime(now) {
     minutes = `0${minutes}`;
   }
   let timeDay = `${day} ${hour}:${minutes}`;
-  return timeDay;
+  let timeIs = document.querySelector("#time-of-day");
+  timeIs.innerHTML = `${timeDay}`;
+  // return timeDay;
+  // let timeDay = currentTime(now);
+}
+function updateForecast(response) {
+  // console.log(response);
+  console.log(response.data);
+}
+function forecastSearch(city) {
+  let cityName = city;
+  let apiKey = "f3a9oa3363ft3b740c40b00ab384f6d4";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${apiKey}`;
+  console.log(apiURL);
+  axios.get(apiURL).then(updateForecast);
 }
 
 function updateTemperature(response) {
-  console.log(response.data.condition.icon_url);
+  // console.log(response.data.condition.icon_url);
+
   let cityName = document.querySelector("#chosen-city");
   cityName.innerHTML = `${response.data.city}`;
-  document.querySelector("#current-temperature-value").innerHTML = Math.round(
-    response.data.temperature.current
-  );
+
+  celsiusTemp = Math.round(response.data.temperature.current);
+  document.querySelector(
+    "#current-temperature-value"
+  ).innerHTML = `${celsiusTemp}`;
   document.querySelector(
     "#feels-like"
   ).innerHTML = `Feels Like ${response.data.temperature.feels_like} °C`;
@@ -44,20 +79,23 @@ function updateTemperature(response) {
   document.querySelector(
     "#wind"
   ).innerHTML = `Wind: ${response.data.wind.speed}km/hr`;
+
   let weatherImage = document.querySelector("#main-icon");
   weatherImage.setAttribute("src", `${response.data.condition.icon_url}`);
   weatherImage.setAttribute("alt", `${response.data.condition.description}`);
-  // let timeIs = document.querySelector("#time-of-day");
-  // timeIs.innerHTML = currentTime(response.data.time * 1000);
-  // document.querySelector(
-  //   ".main-icon"
-  // ).innerHTML = <img src = `${response.data.condition.icon_url}` >  ${response.data.icon}.png`
+  let timePlace = response.data.time * 1000;
+  // console.log(timePlace);
+  currentTime(timePlace);
+
+  // Forecast Info - not working
+  let city = `${response.data.city}`;
+  forecastSearch(city);
 }
 
 function search(city) {
   let apiKey = "f3a9oa3363ft3b740c40b00ab384f6d4";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiURL);
+  // console.log(apiURL);
 
   axios.get(apiURL).then(updateTemperature);
 }
@@ -69,7 +107,7 @@ function updateCity(event) {
 }
 
 function locationData(position) {
-  console.log(position);
+  // console.log(position);
   let apiKey = "f3a9oa3363ft3b740c40b00ab384f6d4";
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
@@ -79,19 +117,21 @@ function locationData(position) {
   axios.get(apiURL).then(updateTemperature);
 }
 function getPosition() {
-  console.log("yay");
+  // console.log("yay");
   navigator.geolocation.getCurrentPosition(locationData);
 }
-
-let now = new Date();
-let timeDay = currentTime(now);
-let timeIs = document.querySelector("#time-of-day");
-timeIs.innerHTML = `${timeDay}`;
 
 let cityForm = document.querySelector("#searchcity");
 cityForm.addEventListener("submit", updateCity);
 
 let locationButton = document.querySelector(".current-location");
 locationButton.addEventListener("click", getPosition);
+
+let farenhiteLink = document.querySelector("#farenhite-link");
+farenhiteLink.addEventListener("click", farenhiteConversion);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", celsiusConversion);
+
+let celsiusTemp = null;
 
 search("Narrabri");
